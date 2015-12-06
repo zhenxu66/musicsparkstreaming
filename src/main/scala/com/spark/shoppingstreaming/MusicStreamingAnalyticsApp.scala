4833a75434cd06308288dcc207d0996fe610e41b
zhen <zhenxu66@gmail.com>
2015-12-05 21:48:36 -0600

    first commit

--- src/main/scala/com/spark/shoppingstreaming/MusicStreamingAnalyticsApp.scala
+++ src/main/scala/com/spark/shoppingstreaming/MusicStreamingAnalyticsApp.scala
@@ -17,7 +17,7 @@ object MusicStreamingAnalyticsApp {
 
     def main(args: Array[String]) {
 
-    val ssc = new StreamingContext("local[2]", "First Streaming App", Seconds(10))
+    val ssc = new StreamingContext("local[2]", "Michael Music Streaming App", Seconds(10))
     val stream = ssc.socketTextStream("localhost", 9999)
 
     // create stream of events from raw text elements
